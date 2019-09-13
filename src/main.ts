5d1cc6a9b788ee1dba19ba6f488f00cb51bd2bfe
Hari Anugrah <code.anugrah@gmail.com>
2019-09-13 20:43:12 +0800

    working nestjs wrapped inside electron apps

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { WsAdapter } from "@nestjs/platform-ws";
+import { app, BrowserWindow } from "electron";
+import * as path from "path";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.useWebSocketAdapter(new WsAdapter(app));
-  await app.listen(3000);
+  const nestApp = await NestFactory.create(AppModule);
+  nestApp.useWebSocketAdapter(new WsAdapter(nestApp));
+  await nestApp.listen(3000);
 }
-bootstrap();
+
+function createWindow() {
+  bootstrap();
+  // Create the browser window.
+  let win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  // and load the index.html of the app.
+  win.loadFile(path.join(__dirname, "../index.html"));
+}
+
+app.on('ready', createWindow)
